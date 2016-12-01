0d0d4382e34d7165ea01d064af573c7fab04e4c7
D.Lo <WebDevDLo@gmail.com>
2016-12-01 14:53:35 -0500

    Add connections to database and Eloquent ORM.

--- src/settings.php
+++ src/settings.php
@@ -4,6 +4,23 @@
  * @author Digital D.Lo <WebDevDLo@gmail.com>
  */
 
+ /** @todo Set sensitive variables during build in a file that is not committed to sourcecode */
  return $settings = [
-    'displayErrorDetails' => true
+    'displayErrorDetails' => true,
+    'db' => [ // For PDO connection
+        'host' => 'localhost',
+        'dbname' => 'todo',
+        'user' => 'todo',
+        'pass' => 'temporary'
+        ],
+    'orm' => [ // For Eloquent ORM
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'todo',
+        'username' => 'todo',
+        'password' => 'temporary',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+        ]
     ];
