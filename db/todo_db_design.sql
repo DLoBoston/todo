29a2c7b833bbd035fbe1b878cf3303cc21f4af9e
D.Lo <WebDevDLo@gmail.com>
2016-12-03 07:16:18 -0500

    Major update to db design. Add tables and relationships needed for full application. Also add workbench ERD for documentation purposes.

--- db/todo_db_design.sql
+++ db/todo_db_design.sql
@@ -16,17 +16,67 @@
 CREATE DATABASE IF NOT EXISTS `todo` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `todo`;
 
+-- Dumping structure for table todo.comments
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_comments_tasks` (`task_id`),
+  CONSTRAINT `FK_comments_tasks` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table todo.files
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `path` varchar(500) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_files_tasks` (`task_id`),
+  CONSTRAINT `FK_files_tasks` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table todo.subtasks
+DROP TABLE IF EXISTS `subtasks`;
+CREATE TABLE IF NOT EXISTS `subtasks` (
+  `id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `order` int(11) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_subtasks_tasks` (`task_id`),
+  CONSTRAINT `FK_subtasks_tasks` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
 -- Dumping structure for table todo.tasks
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `list_id` int(11) NOT NULL,
   `description` varchar(250) NOT NULL,
+  `notes` text NOT NULL,
   `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `starred` tinyint(1) NOT NULL DEFAULT '0',
+  `assignment_id` int(11) DEFAULT NULL,
+  `due_date` date NOT NULL,
+  `order` int(11) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_tasks_todo_lists` (`list_id`),
+  KEY `FK_tasks_users` (`assignment_id`),
+  CONSTRAINT `FK_tasks_users` FOREIGN KEY (`assignment_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_tasks_todo_lists` FOREIGN KEY (`list_id`) REFERENCES `todo_lists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
@@ -35,7 +85,25 @@ CREATE TABLE IF NOT EXISTS `tasks` (
 DROP TABLE IF EXISTS `todo_lists`;
 CREATE TABLE IF NOT EXISTS `todo_lists` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) NOT NULL,
   `name` varchar(150) NOT NULL,
+  `order` int(11) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_todo_lists_users` (`owner_id`),
+  CONSTRAINT `FK_todo_lists_users` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table todo.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
