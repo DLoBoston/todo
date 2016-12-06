a2251a34eed2e3fa864a17690e397cc4437bd839
D.Lo <WebDevDLo@gmail.com>
2016-12-06 12:22:55 -0500

    Add processLogin function to SiteController.

--- src/ToDo/Controllers/SiteController.php
+++ src/ToDo/Controllers/SiteController.php
@@ -21,9 +21,14 @@ class SiteController extends Controller {
 	 * @return \Slim\Http\Response $response PSR-7 Response
 	 */
 	public function showLogin(Request $request, Response $response)
-	{		
+	{	
+		// Get URI object for route to be passed to template
+		$uri = $request->getUri();
+    
 		// Return template
-		$response = $this->container->get('view')->render($response, "login.php", ['page_title' => 'Login']);
+		$response = $this->container->get('view')->render($response, "login.php",
+      ['page_title' => 'Login',
+       'route' => $uri->getPath()]);
 		return $response;
 	}
 	
@@ -44,6 +49,35 @@ class SiteController extends Controller {
 		// Return template
 		$response = $this->container->get('view')->render($response, "home.php", ['page_title' => 'Current Lists', 'lists' => $lists]);
 		return $response;
-	}  
+	}
+  
+  /**
+   * Process a login attempt. Redirect based on success or failure.
+	 * 
+	 * @param \Slim\Http\Request $request PSR-7 Request
+	 * @param \Slim\Http\Response $response PSR-7 Response
+   */
+  public function processLogin(Request $request, Response $response)
+  {
+    // Get submission
+		$data = $request->getParsedBody();
+    
+    // Validate submission
+    $login_controller = new \ToDo\Controllers\LoginController($this->container);
+    $login_results = $login_controller->validateLogin($data);
+    
+    // If valid...
+    if ($login_results['valid_submission']) {
+      // Set session
+      // Redirect to home
+      redirect_to('/');
+    }
+    // Else...
+    else {
+      // Redirect to login
+      $uri = $request->getUri(); // Get URI object for route to be passed to redirect
+      redirect_to($uri->getPath());
+    }
+  }
   
 }
