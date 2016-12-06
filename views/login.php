fcdf7b727399a0447cd25a31f71fc7cd3c627366
D.Lo <WebDevDLo@gmail.com>
2016-12-06 12:21:14 -0500

    Add form markup to login template.

--- views/login.php
+++ views/login.php
@@ -4,6 +4,14 @@
   
 	<?php include 'partials/header.php'; ?>
   
-	<!-- Code goes here -->
+  <form action="<?= htmlspecialchars($route, ENT_QUOTES, "utf-8"); ?>" method="post">
+    
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email">
+    
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <input type="submit" value="Login">
+  </form>
 
 <?php include 'partials/html_footer.php';
